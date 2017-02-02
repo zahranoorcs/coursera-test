c216135e3cecde44372aa45a1c0682f1d93f3d3d
Zahra Noor <zahranoor@Zahras-MacBook-Pro.local>
2017-02-02 15:45:10 +0900

    final assignment submission

---
+++ angular_week1/javascript/app.js
@@ -0,0 +1,27 @@
+(function(){
+'use strict';
+angular.module('myApp',[])
+       .controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+
+function LunchCheckController($scope){
+         $scope.menu="";
+$scope.lunch_checker = function(){
+      var data = $scope.menu;
+      if(data == ""){
+          $scope.message = "Please enter data first";
+      }
+      else {
+        var splitted_data = data.split(",");
+        if(splitted_data.length > 3){
+          $scope.message = "Too much!";
+        }
+        else{
+          $scope.message = "Enjoy";
+        }
+      }
+};
+     }
+
+})();
