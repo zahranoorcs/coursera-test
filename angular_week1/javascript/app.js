a39f74275662154b7b15f8267f7ade56477277e9
Zahra Noor <zahranoor@Zahras-MacBook-Pro.local>
2017-02-02 19:35:01 +0900

    try

--- angular_week1/javascript/app.js
+++ angular_week1/javascript/app.js
@@ -1,29 +1,28 @@
 (function(){
 'use strict';
 angular.module('LunchCheck',[])
-       .controller('LunchCheckController', LunchCheckController);
+       .controller('LunchCheckController', function($scope){
+         $scope.message="";
+         $scope.menu="";
+                $scope.lunch_checker = function(){
 
-LunchCheckController.$inject = ['$scope'];
+             var data = $scope.menu;
+             if(data == ""){
+                 $scope.message = "Please enter data first";
+             }
+             else {
+               var splitted_data = data.split(",");
+               if(splitted_data.length > 3){
+                 $scope.message = "Too much!";
+               }
+               else{
+                 $scope.message = "Enjoy";
+               }
+             }
+       }
+       });
 
-function LunchCheckController($scope){
-  $scope.message="";
-  $scope.menu="";
-         $scope.lunch_checker = function(){
+// LunchCheckController.$inject = ['$scope'];
 
-      var data = $scope.menu;
-      if(data == ""){
-          $scope.message = "Please enter data first";
-      }
-      else {
-        var splitted_data = data.split(",");
-        if(splitted_data.length > 3){
-          $scope.message = "Too much!";
-        }
-        else{
-          $scope.message = "Enjoy";
-        }
-      }
-}
-     }
 
 })();
