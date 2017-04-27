aeb2497aa1c97f20f6ec1d7f604bee1cf8e28cdf
Zahra Noor <zahranoor@Zahras-MacBook-Pro.local>
2017-04-27 18:40:53 -0400

    d

---
+++ angular_customer_project/app.js
@@ -0,0 +1,6 @@
+//ng-route =>If you want to navigate to different pages in your application, but
+//you also want the application to be a SPA (Single Page Application),
+//with no page reloading, you can use the ngRoute module.
+
+
+angular.module("CrudDemoApp",["CrudDemoApp.controllers","ngRoute"]);
---
+++ angular_customer_project/controllers.js
@@ -0,0 +1,4 @@
+angular.module("CrudDemoApp.controllers",[]).
+controller("MainController",function($scope){
+  $scope.message = "Main Controller";
+})
