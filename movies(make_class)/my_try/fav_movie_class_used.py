369330a010c4ef71574074ced045d02cb7aef8fd
Zahra Noor <zahranoor@Zahras-MacBook-Pro.local>
2017-06-05 10:05:23 -0400

    upload plz

--- movies(make_class)/my_try/fav_movie_class_used.py
+++
@@ -1,7 +0,0 @@
-import fav_movie_new_class
-shawshank_redemption=fav_movie_new_class.Movie("shawshank redemption",
-                                     "Andy Dufresne (Tim Robbins) is sentenced to two consecutive life terms in prison for the murders of his wife and her lover and is sentenced to a tough prison. However, only Andy knows he didn't commit the crimes. While there, he forms a friendship with Red (Morgan Freeman), experiences brutality of prison life, adapts, helps the warden, etc., all in 19 years.",
-                                     "https://www.google.com/search?q=shawshank+redemption+poster&safe=active&espv=2&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjhre6LxLTTAhUBzxQKHeJSDRUQ_AUIBigB&biw=1282&bih=635#imgrc=e8uMeMlsgSK-_M:",
-                                     "https://www.youtube.com/watch?v=6hB3S9bIaco")
-print(shawshank_redemption.movie_storyline)
-shawshank_redemption.show_trailer()
