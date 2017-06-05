bbeb3e643f37fec82b6eb3d90e1566394bbe43c9
zahranoorcs <zahranoorcs1514@gmail.com>
2017-06-04 21:55:37 -0400

    Update fresh_tomatoes.py

--- movies(make_class)/fresh_tomatoes.py
+++ movies(make_class)/fresh_tomatoes.py
@@ -10,7 +10,8 @@ main_page_head = '''
 
     <!-- Bootstrap 3 -->
     
-    <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/jquery-ui.min.js" type="text/javascript"></script>
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+
 
     <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap-theme.min.css">
